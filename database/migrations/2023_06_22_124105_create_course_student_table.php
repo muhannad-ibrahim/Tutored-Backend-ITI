8af8f8155b556c84748c90bb4af99e74ec9b5491
Mahmoudawd4 <mahmoudawd54@gmail.com>
2023-06-24 15:35:10 +0300

    create migartions exams and corse course_student and course contents and student_subject_exam

---
+++ database/migrations/2023_06_22_124105_create_course_student_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_student', function (Blueprint $table) {
+            $table->foreignId('course_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+
+            $table->foreignId('student_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+
+            $table->primary(['course_id','student_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_student');
+    }
+};
---
+++ database/migrations/2023_06_22_124141_create_student_subject_exam_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_subject_exam', function (Blueprint $table) {
+                 $table->integer('degree');
+                 $table->foreignId('exam_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+                 $table->foreignId('student_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+                 $table->primary(['exam_id','student_id']);
+                 $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_subject_exam');
+    }
+};
