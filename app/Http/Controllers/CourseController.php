936e6b2f97316b61dee7379d700f948fc22bfe59
Mahmoudawd4 <mahmoudawd54@gmail.com>
2023-06-26 01:46:41 +0300

    create  functions show video in   course controller

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -162,6 +162,20 @@ class CourseController extends Controller
     }
 
 
+    public function showvideo($e_id)
+    {
+
+
+        $course = DB::select("select * from course__contents where course_id = $e_id");
+        if ($course) {
+
+            return response()->json($course, 200);
+        }
+        return response()->json("Not Found", 404);
+    }
+
+
+    
 
 
 
