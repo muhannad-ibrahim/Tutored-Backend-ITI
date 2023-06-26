aa7abedd909954f82dcf5ec767fd02b09c077329
Mohammed Abdallah <mohammed.a.farhoud.com>
2023-06-26 12:31:52 +0300

    Modifying exams and qustions database tables to separate answers into another table

--- database/migrations/2023_06_22_202149_create_questions_table.php
+++ database/migrations/2023_06_22_202149_create_questions_table.php
@@ -6,36 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateQuestionsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
-
             $table->string('header');
-            $table->string('choice_1');
-            $table->string('choice_2');
-            $table->string('choice_3');
-            $table->string('choice_4');
-            $table->string('answer');
             $table->integer('score');
             $table->foreignId('exam_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
-            
+            $table->timestamps();
+        });
+
+        Schema::create('choices', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->boolean('is_correct')->default(false);
+            $table->foreignId('question_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
+        Schema::dropIfExists('choices');
         Schema::dropIfExists('questions');
     }
 }
