e82a02645eaf1a54bd6ea6a072c0ca1a0def09c9
Mahmoudawd4 <mahmoudawd54@gmail.com>
2023-06-26 20:39:31 +0300

    make seed and factory seed

---
+++ database/seeders/Course_ContentSeeder.php
@@ -0,0 +1,169 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+
+class Course_ContentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('course__contents')->insert([
+            'name' => 'week 1',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 1
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 2',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 1
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 3',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 1
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 1',
+            'content' => 'https://www.youtube.com/embed/cpP-fCo8Dn4',
+            'course_id' => 2
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 2',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 2
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 3',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 2
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 1',
+            'content' => 'https://www.youtube.com/embed/cpP-fCo8Dn4',
+            'course_id' => 3
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 2',
+            'content' => 'https://www.youtube.com/embed/cpP-fCo8Dn4',
+            'course_id' => 3
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 3',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 3
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 1',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 4
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 2',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 4
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 3',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 4
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 1',
+            'content' => 'https://www.youtube.com/embed/cpP-fCo8Dn4',
+            'course_id' => 5
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 2',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 5
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 3',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 5
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 1',
+            'content' => 'https://www.youtube.com/embed/cpP-fCo8Dn4',
+            'course_id' => 6
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 2',
+            'content' => 'https://www.youtube.com/embed/cpP-fCo8Dn4',
+            'course_id' => 6
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 3',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 6
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 1',
+            'content' => 'https://www.youtube.com/embed/cpP-fCo8Dn4',
+            'course_id' => 7
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 2',
+            'content' => 'https://www.youtube.com/embed/cpP-fCo8Dn4',
+            'course_id' => 7
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 3',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 7
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 1',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 8
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 2',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 8
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 3',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 8
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 1',
+            'content' => 'https://www.youtube.com/embed/cpP-fCo8Dn4',
+            'course_id' => 9
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 2',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 9
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 3',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 9
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 1',
+            'content' => 'https://www.youtube.com/embed/cpP-fCo8Dn4',
+            'course_id' => 10
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 2',
+            'content' => 'https://www.youtube.com/embed/cpP-fCo8Dn4',
+            'course_id' => 10
+        ]);
+        DB::table('course__contents')->insert([
+            'name' => 'week 3',
+            'content' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            'course_id' => 10
+        ]);
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        $this->call([
+            
+            UserSeeder::class,
+            StudentSeeder::class,
+            TrainerSeeder::class,
+            CategorySeeder::class,
+            CourseSeeder::class,
+            Course_ContentSeeder::class,
+            ExamSeeder::class,   
+            questionSeeder::class,
+            feedbackSeeder::class,
+            contactUsSeeder::class,
+            
+        ]);
+    }
+}
